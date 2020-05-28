03c24a51975b64315dcd85fe952b68d5b7a276df
ninja <ninja@localhost.localdomain>
2020-05-28 12:24:04 +0530

    update

--- data.py
+++ data.py
@@ -7,5 +7,6 @@ os.system("wget https://api.covid19api.com/summary")
 time.sleep(5)
 os.system("mv summary summary.json")
 os.system("git push -u origin master")
-
+os.system("git add .")
+os.system("git commit -m "update" ")
 #incomplete scipt
